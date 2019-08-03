7941897ee2b1ded60a3ad527785915ed5aace803
Amardip Kumar <amardip.kumar@ndi-mb-005.local>
2019-08-03 23:05:56 +0530

    changes

--- test.py
+++ test.py
@@ -1,46 +1,46 @@
-# class A(object):
-#     # def __init__(self):
-#     #     print "A init"
+class A(object):
+    # def __init__(self):
+    #     print "A init"
 
-#     def foo(self):
-#         print "A"
+    def foo(self):
+        print "A"
     
 
-# class B(object):
-#     def __init__(self):
-#         print "B init"
+class B(object):
+    def __init__(self):
+        print "B init"
 
-#     def foo(self):
-#         print "B"
+    def foo(self):
+        print "B"
 
 
-# class E(B):
-#     def __init__(self):
-#         super(E, self).__init__()
-#         print "E init"
+class E(B):
+    def __init__(self):
+        super(E, self).__init__()
+        print "E init"
 
-#     def foo(self):
-#         super(E, self).foo()
-#         print "E"
+    def foo(self):
+        super(E, self).foo()
+        print "E"
     
-# class D(A,E):
-#     pass
+class D(A,E):
+    pass
 
-# obj = D()
-# obj.foo()
-# print D.__mro__
+obj = D()
+obj.foo()
+print D.__mro__
 
 
 
-# def test(func):
-#     def inner(*args, **kwargs):
-#         print "welcome"
-#         func(*args, **kwargs)
+def test(func):
+    def inner(*args, **kwargs):
+        print "welcome"
+        func(*args, **kwargs)
         
-#     return inner
+    return inner
 
-# @test
-# def test_deco():
-#     print "hi"
-# test_deco()
+@test
+def test_deco():
+    print "hi"
+test_deco()
 
