e48c235e5cb59b2b4bafea99ae9f4b103611cb27
Amardip Kumar <amardip.kumar@ndi-mb-005.local>
2019-09-26 21:18:34 +0530

    changes

--- special_method.py
+++ special_method.py
@@ -1,56 +1,57 @@
 #----------------collection namedtuple---------------------------------
 #---------- A deck as a sequence of cards.---------------------------
 
-# import collections
-# Card = collections.namedtuple('Card', ['rank', ' '])
-# class FrenchDeck:
-#     ranks = [str(n) for n in range(2, 11)] + list('JQKA') 
-#     suits = 'spades diamonds clubs hearts'.split()
+import collections
+Card = collections.namedtuple('Card', ['rank', 'suit'])
+class FrenchDeck:
+    ranks = [str(n) for n in range(2, 11)] + list('JQKA') 
+    suits = 'spades diamonds clubs hearts'.split()
 
-#     def __init__(self):
-#         self._cards = [Card(rank, suit) for suit in self.suits for rank in self.ranks]
-#     def __len__(self):
-#         return len(self._cards)
-#     def __getitem__(self, position): 
-#         return self._cards[position]
+    def __init__(self):
+        self._cards = [Card(rank, suit) for suit in self.suits for rank in self.ranks]
+    def __len__(self):
+        return len(self._cards)
+    def __getitem__(self, position): 
+        return self._cards[position]
 
-# beer_card = Card('7', 'diamonds')
-# deck = FrenchDeck()
+beer_card = Card('7', 'diamonds')
+deck = FrenchDeck()
 
-# suit_values = dict(spades=0, hearts=1, diamonds=2, clubs=3)
+suit_values = dict(spades=0, hearts=1, diamonds=2, clubs=3)
 
-# def spades_high(card):
-#     rank_value = FrenchDeck.ranks.index(card.rank)
-#     return rank_value * len(suit_values) + suit_values[card.suit]
+def spades_high(card):
+    rank_value = FrenchDeck.ranks.index(card.rank)
+    return rank_value * len(suit_values) + suit_values[card.suit]
 
-# for card in sorted(deck, key = spades_high):
-#     print (card)
+for card in sorted(deck, key = spades_high):
+    print (card)
 
 # ---- Vector class implementing the operations just described, through the use of the special methods __repr__, __abs__, __add__ and __mul__:---------
 
-# from math import hypot
-# class Vector:
-#     def __init__(self, x =0 , y= 0):
-#         self.x = x
-#         self.y = y
+from math import hypot
+class Vector:
+    def __init__(self, x =0 , y= 0):
+        self.x = x
+        self.y = y
     
-#     def __repr__(self):
-#         return "Vector(%r, %r)"%(self.x, self.y)
+    def __repr__(self):
+        return "Vector(%r, %r)"%(self.x, self.y)
     
-#     def __abs__(self):
-#         return hypot(self.x, self.y)
+    def __abs__(self):
+        return hypot(self.x, self.y)
 
-#     def __bool__(self):
-#         return bool(abs(self))
+    def __bool__(self):
+        return bool(abs(self))
 
-#     def __add__(self, other):
-#         x = self.x + other.x
-#         y = self.y + other.y
-#         return Vector(x, y)
-#     def __mul__(self, scaler):
-#         return Vector(self.x * scaler , self.y * scaler)
+    def __add__(self, other):
+        x = self.x + other.x
+        y = self.y + other.y
+        return Vector(x, y)
+    def __mul__(self, scaler):
+        return Vector(self.x * scaler , self.y * scaler)
     
-# obj = Vector(4,5)
+obj = Vector(4,5)
+import pdb;pdb.set_trace()
 
 
 #----------------Name Tuple --------------
--- test1.py
+++ test1.py
@@ -1,17 +1,131 @@
-import re
-string = 'h8i rajes34singh h4ow a23re yo76u abc67x'
-split_string = string.split()
-all_int = []
-for i in split_string:
-    num = int(re.search(r'\d+', i).group())
-    all_int.append(num)
-all_int.sort()
+# import re
+# string = 'h8i rajes34singh h4ow a23re yo76u abc67x'
+# split_string = string.split()
+# all_int = []
+# for i in split_string:
+#     num = int(re.search(r'\d+', i).group())
+#     all_int.append(num)
+# all_int.sort()
 
-final_list = []
-for nn in all_int:
-    # dd = [s for s in split_string if str(nn) in s and len(re.search(r'\d+', i).group()) == len(str(nn))]
-    for st in split_string:
-        if str(nn) in st and len(re.search(r'\d+', st).group()) == len(str(nn)):
-            final_list.append("".join(sorted(st)))
-result = " ".join(final_list)
-print "final result is :",result
+# final_list = []
+# for nn in all_int:
+#     # dd = [s for s in split_string if str(nn) in s and len(re.search(r'\d+', i).group()) == len(str(nn))]
+#     for st in split_string:
+#         if str(nn) in st and len(re.search(r'\d+', st).group()) == len(str(nn)):
+#             final_list.append("".join(sorted(st)))
+# result = " ".join(final_list)
+# print "final result is :",result
+
+
+# import bobo
+
+# @bobo.query('/')
+# def hello(name):
+#     return 'Hello %s!' % name
+
+# bobo -f test1.py
+
+
+#-------------attrgetter---------
+
+# metro_data = [
+#  ('Tokyo', 'JP', 36.933, (35.689722, 139.691667)),
+#  ('Delhi NCR', 'IN', 21.935, (28.613889, 77.208889)),
+#  ('Mexico City', 'MX', 20.142, (19.433333, -99.133333)),
+#  ('New York-Newark', 'US', 20.104, (40.808611, -74.020386)),
+#  ('Sao Paulo', 'BR', 19.649, (-23.547778, -46.635833)),  ]
+
+# from collections import namedtuple
+# LatLong = namedtuple('LatLong', 'lat long') 
+# Metropolis = namedtuple('Metropolis', 'name cc pop coord')
+# metro_areas = [Metropolis(name, cc, pop, LatLong(lat, long)) for name, cc, pop, (lat, long) in metro_data]
+# from operator import attrgetter
+# name_lat = attrgetter('name', 'coord.lat')
+# for city in sorted(metro_areas, key=attrgetter('coord.lat')):
+#     print(name_lat(city))
+
+
+# class Test:
+#     cc = 8
+#     def __init__(self,a,b):
+#         self.a = a
+#         self.b = b
+    
+#     # @staticmethod
+#     # def foo(b):
+#     #     c = Test.cc
+#     #     return c
+
+#     @classmethod
+#     def create_from_list(cls, data):
+#         return cls(data[0], data[1])
+        
+
+# obj = Test.create_from_list([3,4])
+# # print obj.foo(6)
+# print obj.a
+
+#-----------------static method----
+# class Test:
+#     def __init__(self,a,b):
+#         self.a = a
+#         self.b = b
+
+#     def _foo1(self):
+#         return self.a
+    
+#     ee = property(_foo1)
+
+#     @staticmethod
+#     def foo():
+#         return "static method parent"
+    
+#     @classmethod
+#     def bar(cls):
+#         return "class method parent"
+
+# class Test1(Test):
+#     # pass
+#     def __init__(self):
+#         self.a = 10
+
+#     @staticmethod
+#     def foo():
+#         return "static method child"
+    
+   
+
+# obj = Test1()
+# print (obj.bar())
+
+# obj = Test(10,7)
+# print obj._foo1()
+
+# obj = Test1()
+# print (obj._foo1())
+
+
+#---------------------
+
+class A(object):
+    pass
+    # def foo(self):
+    #     return "class A"
+    
+
+class B(object):
+    def foo(self):
+        return "class B"
+
+class D(B):
+    def foo(self):
+        super(D, self).foo()
+        return "class C"
+
+class C(A,D):
+    pass
+
+obj = C()
+print (obj.foo())
+print (C.mro())
+
